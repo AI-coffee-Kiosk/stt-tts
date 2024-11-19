a3fee6620d5d37274c1a466cf3f9881feb27921d
cyg0828 <cyg0828@gmail.com>
2024-11-20 02:17:48 +0900

    Add files via upload
    stt

---
+++ mykiosk/mykiosk/src/main/java/com/example/mykiosk/MykioskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mykiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykioskApplication.class, args);
+	}
+
+}
---
+++ mykiosk/mykiosk/src/main/java/com/example/mykiosk/dto/sttForm.java
@@ -0,0 +1,4 @@
+package com.example.mykiosk.dto;
+
+public class sttForm {
+}
