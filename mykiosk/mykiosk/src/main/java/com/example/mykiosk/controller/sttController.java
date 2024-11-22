0332906802b71982c595119bfc84ae18bc24f7c4
cyg0828 <cyg0828@gmail.com>
2024-11-22 15:35:25 +0900

    Commit test

--- mykiosk/mykiosk/src/main/java/com/example/mykiosk/controller/sttController.java
+++ mykiosk/mykiosk/src/main/java/com/example/mykiosk/controller/sttController.java
@@ -14,7 +14,7 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
+//
 @RestController
 @RequestMapping("/api/speech")
 public class sttController {
