b6003255bf3c14b033eb176df3a84caa940c50cc
cyg0828 <cyg0828@gmail.com>
2024-11-24 20:17:46 +0900

    test

--- mykiosk/mykiosk/src/main/java/com/example/mykiosk/controller/ConnectController.java
+++ mykiosk/mykiosk/src/main/java/com/example/mykiosk/controller/ConnectController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/chatbot")
 public class ConnectController {
     @PostMapping("/chat")
-    public String chat(@RequestBody String message) {
+    public String chat(@RequestBody Map<String, String> message) {
 
-        System.out.println(message);
+        System.out.println(message.get("message"));
         return "";
     }
 
